6a07a8d6eacdb5ee52309b6abcfcbacaf27505db
GelsonFilho <gelsonfilho.contato@gmail.com>
2022-09-11 03:08:23 -0300

    Estrutura básica e implementação de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './components/contato/contato.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { MateriaComponent } from './components/materia/materia.component';
+import { RecentesComponent } from './components/recentes/recentes.component';
+import { SobreComponent } from './components/sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: 'contato',
+    component: ContatoComponent
+  },
+  {
+    path: 'inicio',
+    component: InicioComponent
+  },
+  {
+    path: 'materia',
+    component: MateriaComponent
+  },
+  {
+    path: 'recentes',
+    component: RecentesComponent
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { RecentesComponent } from './components/recentes/recentes.component';
+import { MateriaComponent } from './components/materia/materia.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { ContatoComponent } from './components/contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    RecentesComponent,
+    MateriaComponent,
+    SobreComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
